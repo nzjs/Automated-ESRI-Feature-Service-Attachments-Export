cea34944e2016c4a7d9c967e81ec74549ceeb175
John Stowell <stowell.j@live.com>
2019-10-04 14:28:29 +1300

    Multiple survey input, SharePoint Online syncing
    Script amendments to allow for multiple survey inputs and SharePoint Online syncing.

--- S123AttachmentsToFolder.py
+++ S123AttachmentsToFolder.py
@@ -1,68 +1,108 @@
-"""
-This script takes an input ESRI Feature Service, retrieves the online attachments, and exports them to a specified location.
-(It's also compatible with private services by generating a token for the service.)
-Developed by John Stowell, 2018
-
-Call this script with:  python "..\S123AttachmentsToFolder.py"
-
-Attachment URL structure: https://services5.arcgis.com/###/arcgis/rest/services/{FEATURE_NAME}/FeatureServer/0/OBJECTID/attachments/ATTACHMENTID?token=###
-
-Customise these variables in the code below: 
-username, password, dataset, and outFolder
-"""
+################################################################
+# This script takes an input ESRI Feature Service, retrieves the online attachments, stores them on a local server. 
+# Additionally, if you have setup a SharePoint Online location to "Sync" with a local C:\ drive location, the attachments can be 
+# automatically synced to O365 via the OneDrive Sync application. 
+# # Developed by John Stowell, 2019
+#
+# Call this script with     python "..\S123AttachmentsToSharepoint.py"
+#
+# Attachment URL structure: https://services5.arcgis.com/###/arcgis/rest/services/{FEATURE_NAME}/FeatureServer/0/OBJECTID/attachments/ATTACHMENTID?token=###
+# Eg. https://services6.arcgis.com/Whb8vGWSmNkavSpL/ArcGIS/rest/services/service_3c1b58d6f75846c7bd82f3e2c3e71a99/FeatureServer/0/9/attachments/1?token=###
+#
+# Customise these variables in the code below: 
+# username, password, fs_base_*, outfolder_*
+################################################################
 
 import os
+import os.path
 import json
 import urllib
+import urllib.request
 import requests
 import subprocess
 
 # Generate a temporary API token for accessing the REST endpoints
-tokenURL = 'https://www.arcgis.com/sharing/rest/generateToken?f=pjson' 
-payload = { 'username': 'ARCGIS ONLINE USERNAME',
-            'password': 'ARCGIS ONLINE PASSWORD',
-            'referer': 'https://www.arcgis.com'
-            }     # Set POST fields here 
-                    # Eg. &username=user&password=password&referer=https://www.arcgis.com
+def generateToken():
+    try:
+        tokenURL = 'https://www.arcgis.com/sharing/rest/generateToken?f=pjson' 
+        payload = { 'username': 'ARCGIS ONLINE USERNAME',
+                    'password': 'ARCGIS ONLINE PASSWORD',
+                    'referer': 'https://www.arcgis.com'
+                    }     # Set POST fields here 
+                        # Eg. &username=user&password=password&referer=https://www.arcgis.com
 
-tokenResponse = requests.post(tokenURL, data=payload)
-# Enable printing below for testing
-#print(response.text) #TEXT/HTML
-#print(response.status_code, response.reason) #HTTP
-tokenJson = tokenResponse.json()
-token = tokenJson['token']
+        tokenResponse = requests.post(tokenURL, data=payload)
+        # Enable printing below for testing
+        #print(response.text) #TEXT/HTML
+        #print(response.status_code, response.reason) #HTTP
+        tokenJson = tokenResponse.json()
+        return tokenJson['token']
+    except Exception as token_ex:
+        print("\t\t Error generating token : Code " + str(tokenResponse.text) + " : " + str(token_ex))
+        pass
 
-dataset = r"https://services6.arcgis.com/Whb8vGWSmNkavSpL/ArcGIS/rest/services/service_3c1b58d6f75846c7bd82f3e2c3e71a99/FeatureServer/0/"
-s123GeoJSON = dataset + "query?where=objectid+%3D+objectid&outfields=*&f=json&token="
-s123dataURL = s123GeoJSON + token
-attachmentsGeoJSON = dataset + "queryAttachments?objectIds=&globalIds=&definitionExpression=1%3D1&attachmentsDefinitionExpression=&attachmentTypes=&size=&keywords=&resultOffset=&resultRecordCount=&f=pjson&token="
-attachURL = attachmentsGeoJSON + token
-outFolder = r"\\<SERVER>\Extracts\ArcGIS\Survey123\Attachments"  
+token = generateToken()
+print('Token generated: ' + str(token))
+print()
+# Enter the FeatureServer URL that you want to sync/download attachments from, with a trailing slash /.
+# Eg. r"https://services3.arcgis.com/za4HjpHWqnA4SFnH/arcgis/rest/services/service_22d11beafd254fd2852807d4062144bc/FeatureServer/0/"
+# In the example below, I have two separate surveys that I want to sync attachments from. The retrieveAttachments() function below allows us to use mutliple inputs.
+fs_base_WQ = r"<FeatureServer URL # 1>"
+fs_base_RFWL = r"<FeatureServer URL # 2>"
+definitionQuery = r"?objectIds=&globalIds=&definitionExpression=1%3D1&attachmentsDefinitionExpression=&attachmentTypes=&size=&keywords=&resultOffset=&resultRecordCount=&f=pjson&token="
+attachURL_WQ = fs_base_WQ + "queryAttachments" + definitionQuery + str(token)
+attachURL_RFWL = fs_base_RFWL + "queryAttachments" + definitionQuery + str(token)
+outfolder_WQ = r"C:\Users\<user>\<Office 365 org name>\<folder where SharePoint Online is syncing # 1>"
+outfolder_RFWL = r"C:\Users\<user>\<Office 365 org name>\<folder where SharePoint Online is syncing # 2>"
 
-print 'Token generated: ' + token
-print ''
+# Variables commented out below ... 
+# Instead we're relying on the OneDrive sync client for automated syncing between a local C: location and SharePoint Online.
+
+#outfolder_WQ = r"\\<server>\Survey123 Data\S123 Images - Water Quality"
+#outfolder_RFWL = r"\\<server>\Survey123 Data\S123 Images - Rainfall and Water Level"
+#sharepoint_WQ = r"<sharepoint online URL>"
+#sharepoint_RFWL = r"<sharepoint online URL>"
+
+
 ################################################################
 # Do not customise below this line
 ################################################################
 
-# Read the json response from our GeoJSON URL, 
-# then change the JSON string into a JSON object (data)
-jsonResponse = urllib.urlopen(attachURL)
-data = json.loads(jsonResponse.read())
+# Define the retrieveAttachments() function:
+# Read the json response from our GeoJSON URL, change the JSON string into a JSON object (data)
+# Then retrieve the relevant attachments and store in our output folder.
 
-print 'JSON response retrieved and converted'
-print ''
+def retrieveAttachments(attach_url, base_url, out_folder, token):
+    jsonResponse = urllib.request.urlopen(attach_url)
+    data = json.loads(jsonResponse.read())
+    print('JSON response retrieved and converted to object')
+    print()
+    for parentObject in data['attachmentGroups']:
+        for attachmentInfo in parentObject['attachmentInfos']:
+            try:
+                print('OID: ' + str(parentObject['parentObjectId']) + ' (Attachment ID: '+str(attachmentInfo.get('id')) + ')  -  ' + str(attachmentInfo.get('name')))
+                img_url = (str(base_url) + str(parentObject['parentObjectId']) + '/attachments/' + str(attachmentInfo.get('id')) + '?token=' + str(token))
+                file_name = ('OID' + str(parentObject['parentObjectId']) + '-' + str(attachmentInfo.get('name'))) # attachmentInfo.get('globalId')
+                #print('Compiled Image URL:')
+                #print(img_url)
+                if os.path.exists(out_folder+'\\'+file_name):
+                    print('File already exists in output location.')
+                    print()
+                else:
+                    urllib.request.urlretrieve(img_url, out_folder+'\\'+file_name)
+                    print('>>> Downloaded file to server, syncing with SharePoint momentarily.')
+                    print()
+            except Exception as retrieve_ex:
+                print("\t\t Error retrieving image : " + str(parentObject['parentObjectId']) + ' : ' + str(attachmentInfo) + " : " + str(retrieve_ex))
+                continue
 
-for parentObject in data['attachmentGroups']:
-    for attachmentInfo in parentObject['attachmentInfos']:
-        print('ID: '+str(attachmentInfo.get('id')) + '  -  ' + str(attachmentInfo.get('globalId')))
-        print 'Compiled Image URL:'
-        imageURL = (dataset + str(parentObject['parentObjectId']) + '/attachments/' + str(attachmentInfo.get('id')) + '?token=' + token)
-        fileName = ('OID' + str(parentObject['parentObjectId']) + '-' + str(attachmentInfo.get('name')))
-        print imageURL
-        urllib.urlretrieve(imageURL, outFolder+'\\'+fileName)
-        print outFolder+fileName
-        print ''
 
-print 'Finished, exported images to: \n' +  outFolder
-print ''
+# Run the functions to get attachments for our respective survey forms
+retrieveAttachments(attachURL_RFWL, fs_base_RFWL, outfolder_RFWL, token)
+retrieveAttachments(attachURL_WQ, fs_base_WQ, outfolder_WQ, token)
+
+# Attachments downloaded are stored in the local SharePoint sync folder, and will be 
+# synced online automatically as long as our onedrive service is running.
+
+print('Finished')
+print()
